fb7c7a1b6e2561ed9b3643250175cb6b065279c5
Lucy Berger and Lauren Burnett <lauren.e.burnett@gmail.com>
2016-06-08 19:46:43 -0700

    Added scripts for isosceles and equilateral triangle results.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("form#triangleform").submit(function() {
+    event.preventDefault();
+    var side1Input = parseInt($("#side1").val());
+    var side2Input = parseInt($("#side2").val());
+    var side3Input = parseInt($("#side3").val());
+    var result;
+
+    if (side1Input === side2Input && side2Input === side3Input) {
+      result = ("Equilateral")
+    } else if (side1Input === side2Input || side1Input === side3Input || side2Input === side3Input) {
+      result = ("Isosceles")
+    }
+    $("#answer").text(result);
+
+
+  });
+});
